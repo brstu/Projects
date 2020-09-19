2d34c7ef6790150c2b23f072dde788afe5e18cf6
Mark Shestakov <as005424@g.bstu.by>
2020-09-19 14:18:17 +0300

    change "test" project

--- WebStorm/test/index.js
+++ WebStorm/test/index.js
@@ -1,13 +1,22 @@
-var o = {}
+let o = {};
 o.x = 1
 
-var p = Object.create(o)
+let p = Object.create(o);
 p.y = 2
 
-var q = Object.create(p)
+let q = Object.create(p);
 q.z = 3
 
-var s = q.toString()
+let s = q.toString();
 
 console.log(q.x + q.y)
 console.log(s)
+
+console.log("\n##############################################\n")
+
+let unitCircle = {r: 1};
+let c = Object.create(unitCircle);
+c.x=1
+c.y=1
+c.r=2
+console.log('unitCircle.r: ' + unitCircle.r)
